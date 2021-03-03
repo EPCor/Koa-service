c28d79a449c456bee484ff1355893b5cd1725658
zhilidali <zhilidali@qq.com>
2021-03-03 08:56:35 +0800

    add babel node loader

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,12 @@
 const envConfigs = require('dotenv').config().parsed;
-const { inspect } = process.env;
-const common = {
+const commonConfigs = {
   script: './dist/index.js',
   node_args: ['--es-module-specifier-resolution=node'],
   instances: 'max',
   max_memory_restart: '512M',
   autorestart: true,
   max_restarts: 10,
-  watch: ['dist'], // true
-  // ignore_watch: ['node_modules', '.vscode', 'logs', 'docs'],
+  watch: ['dist'],
   merge_logs: true,
   error_file: './logs/err.log',
   out_file: './logs/out.log',
@@ -22,21 +20,31 @@ const common = {
     ...envConfigs,
   },
 };
+const devConfigs = {
+  node_args: commonConfigs.node_args,
+  instances: 1,
+};
+const { inspect, loadBabel } = process.env;
 
+if (inspect) devConfigs.node_args.push('--inspect');
+if (loadBabel) {
+  devConfigs.node_args.push('--experimental-loader=@node-loader/babel');
+  Object.assign(devConfigs, {
+    script: 'src',
+    watch: ['src'],
+  });
+}
 console.log(envConfigs);
 
 module.exports = {
   apps: [
     {
-      ...common,
+      ...commonConfigs,
+      ...devConfigs,
       name: 'service-dev',
-      node_args: inspect
-        ? ['--inspect', ...common.node_args]
-        : common.node_args,
-      instances: 1,
     },
     {
-      ...common,
+      ...commonConfigs,
       name: 'service',
     },
   ],
